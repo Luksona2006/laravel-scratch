3b05f8bda149260a27c21466f7efcc1e11351cd1
Luksona2006 <luka.bakuridze@redberry.ge>
2023-03-26 18:57:52 +0400

    feat: eloquent relationship (category)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,6 +6,11 @@
         <a href="/posts/{{ $post->slug }}">
             <h1>{!! $post->title !!}</h1>
         </a>
+
+        <p>
+            <a href="#">{{ $post->category->name }}</a>
+        </p>
+
         <div>
             {{ $post->excerpt }}
         </div>
