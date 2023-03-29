914675f2903fec373f287dc69eea170c8c3b4c3d
Luksona2006 <luka.bakuridze@redberry.ge>
2023-03-29 22:45:54 +0400

    feat: register with validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -13,10 +14,10 @@ class RegisterController extends Controller
 
     public function store() {
         $attributes = request()->validate([
-            'name' => ['required', 'max:255', 'min:2'],
-            'username' => ['required', 'max:255', 'min:3'],
-            'email' => ['required', 'email','max:255'],
-            'password' => ['required', 'max:255', 'min:7']
+            'name' => ['required','min:2', 'max:255'],
+            'username' => ['required','min:3', 'max:255', Rule::unique('users', 'username')],
+            'email' => ['required','email', 'max:255', Rule::unique('users', 'email')],
+            'password' => ['required','min:7','max:255']
         ]);
 
         User::create($attributes);
