04277e20604625872dfe035765c50bdc3dad6b1a
Luksona2006 <luka.bakuridze@redberry.ge>
2023-03-29 16:50:17 +0400

    refactor: dropdown component

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,14 +9,12 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {       
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-            'categories' => Category::all(),
-            'currentCategory' => Category::firstWhere('slug', request('category'))
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category']))->get()
         ]);
     }
 
     public function show(Post $post) {
-        return view('post', ['post' => $post]);
+        return view('post.show', ['post' => $post]);
     }
 }
