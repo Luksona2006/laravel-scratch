eaf65d0c27347c147581de1469482c00ab5f759c
Luksona2006 <luka.bakuridze@redberry.ge>
2023-03-30 14:15:04 +0400

    style: comment form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,18 +1,20 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" width="60" height="60" alt="profile image" class="rounded-xl">
-    </div>
-
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
-            <p class="text-xs">Posted 
-                <time>{{ $comment->created_at->diffForHumans()}}</time>
-            </p>
-        </header>
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" width="60" height="60" alt="profile image" class="rounded-xl">
+        </div>
     
-    <p>{{ $comment->body}}</p>
-</article>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">Posted 
+                    <time>{{ $comment->created_at->diffForHumans()}}</time>
+                </p>
+            </header>
+        </div>
+        
+        <p>{{ $comment->body}}</p>
+    </article>
+</x-panel>
