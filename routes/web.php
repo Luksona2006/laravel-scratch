60bdfa8b69d6231ee6e90217a6430526f43b463a
Luksona2006 <luka.bakuridze@redberry.ge>
2023-03-30 19:00:20 +0400

    feat: Exstractin a newsletter service

--- routes/web.php
+++ routes/web.php
@@ -6,33 +6,12 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('newsletter', function() {
-    request()->validate(['email' => ['required', 'email']]);
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us21'
-    ]);
-
-    try {
-        $response = $mailchimp->lists->addListMember('d04a3aa0c5', [
-            'email_address' => request('email'),
-            'status' => 'subscribed'
-        ]);
-    } catch(\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.'
-        ]);
-    };
-    
-    return redirect('/')->with('success', 'You are now signed up for out newsletters!');
-});
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
