ba523da4df152c7b3e2b6f0b2d0d8f3ce40afc1b
Luksona2006 <luka.bakuridze@redberry.ge>
2023-03-31 20:09:01 +0400

    refactor: better styling & using components

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,7 +1,7 @@
-@props(['name', 'type' => 'text'])
+@props(['name'])
 
 <x-form.field>
     <x-form.label name="{{ $name }}"/>
-    <input type="{{ $type }}" value="{{ old($name) }}" name="{{ $name }}" id="{{ $name }}" required class="border border-gray-400 p-2 w-full">
+    <input value="{{ old($name) }}" name="{{ $name }}" id="{{ $name }}" required class="border border-gray-200 p-2 w-full rounded" {{ $attributes }}>
     <x-form.error name="{{ $name }}"/>
 </x-form.field>
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -2,6 +2,6 @@
 
 <x-form.field>
     <x-form.label name="{{ $name }}"/>
-    <textarea type="text" name="{{ $name }}" id="{{ $name }}" required class="border border-gray-400 p-2 w-full">{{ old($name) }}</textarea>
+    <textarea type="text" name="{{ $name }}" id="{{ $name }}" required class="border border-gray-200 p-2 w-full rounded">{{ old($name) }}</textarea>
     <x-form.error name="{{ $name }}"/>
 </x-form.field>
