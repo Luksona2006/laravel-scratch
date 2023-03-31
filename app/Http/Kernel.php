561cd1f4ac4dd1c577f800a26df92879a0444f15
Luksona2006 <luka.bakuridze@redberry.ge>
2023-03-31 22:44:07 +0400

    refactor: project cleanup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,9 +54,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'admin' => MustBeAdministrator::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
